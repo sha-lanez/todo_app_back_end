0331e2060662b90d12308cde7ce098ac59d7d688
shamarr thompson <shamarr.thompson@aecicharterhs.org>
2017-09-30 19:16:35 +0000

    new improved ruby

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,19 @@
 class TodoController < ApplicationController
     def index
-    
+    @number=10
     end
     def show
+        
+        if params[:id]=='1'
+            @task=" do geometry homework"
+        elsif params[:id]=='2'
+            @task="do ela homework"
+        elsif params[:id]=='3'
+            @task="do living environment hw"
+        
+            
+            
+        end
+        
     end 
 end

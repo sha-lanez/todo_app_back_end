ddee931876fab5ddbd739bdeb6f2e2ccaf326dd6
shamarr thompson <shamarr.thompson@aecicharterhs.org>
2017-10-01 14:32:13 +0000

    new improvements

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,10 +6,13 @@ class TodoController < ApplicationController
         
         if params[:id]=='1'
             @task=" do geometry homework"
+            @taskdescribtion="do worksheet 3 and study for exam on tuesday"
         elsif params[:id]=='2'
             @task="do ela homework"
+            @taskdescribtion="finish reading chapter 29 and finish worksheet"
         elsif params[:id]=='3'
             @task="do living environment hw"
+             @taskdescribtion="finish lab and watch video"
         
             
             

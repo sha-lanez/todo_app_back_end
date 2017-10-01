0d17e04072246b7359a5b11f96dc66abb99d947e
shamarr thompson <shamarr.thompson@aecicharterhs.org>
2017-10-01 19:06:31 +0000

    improved everything

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,20 +3,18 @@ class TodoController < ApplicationController
     @number=10
     end
     def show
-        
-        if params[:id]=='1'
-            @task=" do geometry homework"
-            @taskdescribtion="do worksheet 3 and study for exam on tuesday"
-        elsif params[:id]=='2'
-            @task="do ela homework"
-            @taskdescribtion="finish reading chapter 29 and finish worksheet"
-        elsif params[:id]=='3'
-            @task="do living environment hw"
-             @taskdescribtion="finish lab and watch video"
-        
-            
-            
-        end
+      @todo=Todo.find(params[:id])
         
     end 
+    def new
+    end
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.order = params['order']
+        t.name=params["name"]
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+
 end
